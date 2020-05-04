82de69d6a4c7b72f967961cf0be8f58c4d5f3a28
NachoFillol <55722815+NachoFillol@users.noreply.github.com>
2020-05-04 15:43:39 -0300

    commit

---
+++ controllers/auth.js
@@ -0,0 +1,50 @@
+const jwt = require('../services/jwt');
+const moment = require('moment');
+const User = require('../models/user');
+
+function willExprireToken(token) {
+    const {exp} = jwt.decodeToken(token);
+    const currentDate = moment().unix();
+
+    if(currentDate > exp) {
+        return true;
+    }
+    return false;
+
+}
+
+function refreshAccessToken(req, res) {
+    
+    const {refreshToken} = req.body;
+
+    const ifTokenExpired = willExprireToken(refreshToken);
+
+    if(ifTokenExpired) {
+        res.status(404).send({ message: 'El refreshToken ha expirado' });
+    } else {
+        const { id } = jwt.decodeToken(refreshToken);
+
+        User.findOne({_id: id}, (err, userStored) => {
+
+            if(err) {
+                res.status(500).send({ message: 'Error del servidor' });
+            } else {
+                if (!userStored) {
+                    res.status(404). send({ message: 'Usuario no encontrado' });
+                } else {
+                    res.status(200).send({ 
+                        accessToken: jwt.createAccessToken(userStored),
+                        refreshToken: refreshToken 
+                    });
+                }
+            }
+
+        });
+
+    }
+    
+}
+
+module.exports = {
+    refreshAccessToken
+};
